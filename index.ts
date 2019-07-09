ad6a75aa1652a098fc14ea152aa2533e59eeb789
Beksultan Torokulov <beksultan.torokulov@zensoft.io>
2019-07-09 17:33:22 +0600

    - updated exports;

--- index.ts
+++ index.ts
@@ -14,6 +14,8 @@ export * from './src/core/exceptions/http'
 export * from './src/core/exceptions/request'
 
 export { default as BaseService } from './src/core/service/base'
+export * from './src/core/service/amqp'
+export * from './src/core/service/index'
 
 export { default as autoImport } from './src/core/utils/auto-import'
-export * from './src/core/utils/request'
+export { default as Request } from './src/core/utils/request'
